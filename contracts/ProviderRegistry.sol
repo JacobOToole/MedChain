0b76f90cf5063acb788fb6e41f1e46ceabd7664f
Jacob O'Toole <jacobotoole1403@gmail.com>
2026-05-20 23:31:33 +1000

    ProviderRegistry functions

--- contracts/ProviderRegistry.sol
+++ contracts/ProviderRegistry.sol
@@ -14,8 +14,7 @@ contract ProviderRegistry {
     address[] private providerList;
 
     event ProviderRegistered(address indexed provider, string name, uint256 timestamp);
-    event ProviderRevoked(address indexed provider, uint256 timestamp);
-    event AdminTransferred(address indexed from, address indexed to);
+        event ProviderRevoked(address indexed provider, uint256 timestamp);
 
     modifier onlyAdmin() {
         require(msg.sender == hospitalAdmin, "ProviderRegistry: not hospital admin");
@@ -27,7 +26,35 @@ contract ProviderRegistry {
         hospitalAdmin = _hospitalAdmin;
     }
 
-    // TODO: functions
-    // registerProvider, revokeProvider, isProvider, getProvider
+    function registerProvider(address providerAdd, string calldata name) external onlyAdmin {
+        require(providerAdd != address(0), "ProviderRegistry: zero address");
+        bool isNew = providers[providerAdd].wallet == address(0);
 
+        providers[providerAdd] = Provider({
+            wallet: providerAdd,
+            name: name,
+            active: true
+        });
+
+        if (isNew) providerList.push(providerAdd);
+        emit ProviderRegistered(providerAdd, name, block.timestamp);
+    }
+
+    function revokeProvider(address providerAdd) external onlyAdmin {
+        require(isActiveProvider(providerAdd), "Provider Registry: provider not active");
+        providers[providerAdd].active = false;
+        emit ProviderRevoked(providerAdd, block.timestamp);
+    }
+
+    function isActiveProvider(address addr) public view returns (bool) {
+        return providers[addr].active;
+    }
+
+    function getProvider(address addr) external view returns (Provider memory) {
+        return providers[addr];
+    }
+
+    function providerCount() external view returns (uint256) {
+        return providerList.length;
+    }
 }
